2e902d293d6ececb4ec57af556d696cd25787fce
HimmatEdysor <himmat@edysor.in>
2025-11-24 15:26:43 +0530

    update revert

--- src/app.js
+++ src/app.js
@@ -15,13 +15,8 @@ const userRoutes = require("./routes/user.routes");
 const dashboardRoutes = require("./routes/dashboard.routes");
 const notificationRoutes = require("./routes/notification.routes");
 const app = express();
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 mongoose
